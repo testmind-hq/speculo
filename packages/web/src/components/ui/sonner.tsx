42f8fd7d2108a9c3c441cf4fe043d979fab56666
yuchou87 <yuchou87@gmail.com>
2026-04-05 11:50:11 +0800

    fix: use custom ThemeProvider in sonner instead of next-themes

--- packages/web/src/components/ui/sonner.tsx
+++ packages/web/src/components/ui/sonner.tsx
@@ -5,13 +5,13 @@ import {
   OctagonX,
   TriangleAlert,
 } from "lucide-react"
-import { useTheme } from "next-themes"
+import { useTheme } from "@/components/ThemeProvider"
 import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme } = useTheme()
 
   return (
     <Sonner
